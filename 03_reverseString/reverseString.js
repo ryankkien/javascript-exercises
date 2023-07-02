b27ae4f97c1820f05cd26a0b725104f19e782290
ryankkien <ryankien@ucla.edu>
2023-07-02 15:49:39 -0700

    filled out reverseString.js so that it passes reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let temp = "";
+    for(let i = string.length - 1; i >= 0; i--){
+        temp = temp + string[i];
+    }
+    return temp;
 };
 
 // Do not edit below this line
